c7d29555af08c10af27bcf8c773167249cc3e024
tester automatyczny <magdalena.tos92@gmail.com>
2021-05-18 18:14:38 +0200

    Add Zadanie 2

---
+++ src/Zadanie2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Proszę podaj swój wiek");
+        int wiek = scanner.nextInt();
+        if (wiek >= 18) {
+            System.out.println("Możesz kupić alkohol");
+        } else if (wiek < 0) {
+            System.out.println(" To nieprawidłowa liczba");
+        } else {
+            System.out.println("Spdaka! Nie kupisz alkoholu gnoju");
+        }
+    }
+}
