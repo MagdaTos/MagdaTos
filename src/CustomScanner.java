f9706755ee5a2e7f94b76fbfe1f7d23038c44de1
Magdalena Toś <magdalena.tos92@gmail.com>
2022-03-12 17:43:06 +0100

    little changes

--- src/CustomScanner.java
+++ src/CustomScanner.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class CustomScanner {
-    /*public static void main(String[] args) {
+/*    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj proszę swoje imię.");
         String name = scanner.nextLine();
